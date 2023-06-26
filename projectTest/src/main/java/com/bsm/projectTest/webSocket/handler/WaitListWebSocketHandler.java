28fbd29db5bede861c97e4f74238284b9661433b
SeongMin <SeongMin@DESKTOP-90HPD59>
2023-06-26 11:55:50 +0900

    Fix: 웹소켓 연결 시 세션 저장 하여 연결된 모든 세션에 값을 주도록 수정

--- projectTest/src/main/java/com/bsm/projectTest/webSocket/handler/WaitListWebSocketHandler.java
+++ projectTest/src/main/java/com/bsm/projectTest/webSocket/handler/WaitListWebSocketHandler.java
@@ -1,6 +1,7 @@
 package com.bsm.projectTest.webSocket.handler;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,29 +12,33 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class WaitListWebSocketHandler extends TextWebSocketHandler {
 	
 	//Session 관리를 위한 Map
-		private final Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
-	
+	private final Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
+	private final List<WebSocketSession> webSocketSession;
 	
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log.info("[afterConnectionEstablished] 열림");
 		
-		String memberId = (String) session.getAttributes().get("memberId");
-		log.info("afterConnectionEstablished memberId : " +  memberId);
-		if (memberId != null) {
-			sessionMap.put(memberId, session);
-		}
+		webSocketSession.add(session);
 		
-		sessionMap.forEach((key, value)->{
-			log.info("session map>> id : {} session : {}", key, value);
-		});
+//		String memberId = (String) session.getAttributes().get("memberId");
+//		log.info("afterConnectionEstablished memberId : " +  memberId);
+//		if (memberId != null) {
+//			sessionMap.put(memberId, session);
+//		}
+//		
+//		sessionMap.forEach((key, value)->{
+//			log.info("session map>> id : {} session : {}", key, value);
+//		});
 	}
 	
 	@Override
@@ -46,25 +51,29 @@ public class WaitListWebSocketHandler extends TextWebSocketHandler {
 		//session.sendMessage(new TextMessage(objectMapper.writeValueAsString(payload)));
 //		session.sendMessage(new TextMessage(payload));
 		
+		for(WebSocketSession webSession : webSocketSession) {
+			webSession.sendMessage(message);
+		}
 		
 			// sessionMap.get(memberId)를 통해 memberId에 해당하는 웹소켓 세션(WebSocketSession)을 가져옴
-			WebSocketSession receiverSession = sessionMap.get("rn");
-			// 세션을 찾았다면, 해당 세션으로 데이터를 전송
-			if (receiverSession != null) {
-				try {
-					// trades 리스트를 JSON 문자열로 변환하고, 해당 문자열을 웹소켓 세션으로 전송
-					// objectMapper.writeValueAsString(trades)는 trades 리스트를 JSON 문자열로 변환하는 역할을 수행
-					receiverSession.sendMessage(message);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+//			WebSocketSession receiverSession = sessionMap.get("rn");
+//			// 세션을 찾았다면, 해당 세션으로 데이터를 전송
+//			if (receiverSession != null) {
+//				try {
+//					// trades 리스트를 JSON 문자열로 변환하고, 해당 문자열을 웹소켓 세션으로 전송
+//					// objectMapper.writeValueAsString(trades)는 trades 리스트를 JSON 문자열로 변환하는 역할을 수행
+//					receiverSession.sendMessage(message);
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			}
 		
 	}
 	
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		log.info("[afterConnectionClosed] 닫힘");
+		webSocketSession.remove(session);
 	}
 	
 }
