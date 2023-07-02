16de06bb932114ea3466e6b219169ccb28395598
hbsm0 <hbsm0@DESKTOP-6LFI2N4>
2023-07-02 17:24:20 +0900

    Jwt Fix: NPE를 해결  403 문제 해결 필요

--- projectTest/src/main/java/com/bsm/projectTest/jwt/service/Impl/CustomUserDetailsService.java
+++ projectTest/src/main/java/com/bsm/projectTest/jwt/service/Impl/CustomUserDetailsService.java
@@ -27,7 +27,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 		return jwtDao.findMemberByMemberId(username)
 				.map(this::createUserDetails)
 				.orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다"));
-				
 	}
 	
 	// 해당하는 User의 데이터가 존재한다면 UserDetails 객체로 만들어서 리턴
@@ -37,7 +36,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 		return User.builder()
 				.username(memberDto.getUsername())
 				.password(passwordEncoder.encode(memberDto.getPassword()))
-				.roles(memberDto.getRoles().toArray(new String[0]))
+				.roles(memberDto.getRoles())
 				.build();
 	}
 
